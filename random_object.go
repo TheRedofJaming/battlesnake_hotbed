057000dff0abff197413c08d2033fb01e6049c76
TheRedofJaming <TheBlue.ofJaming@gmx.de>
2022-08-30 01:39:02 +0200

    added 'RandomHead' function to create snake heads

--- random_object.go
+++ random_object.go
@@ -8,10 +8,11 @@ import (
 
 const MAX_BODY_LENGTH = 15
 
-func RandomSnake(name string, available_points map[Point]bool) Snake {
+func RandomSnake(name string, availabePoints []Point) Snake {
 	id := uuid.New().String()
 	health := rand.Intn(100)
-	body := randomBody(rand.Intn(MAX_BODY_LENGTH))
+	head, availablePoints := RandomHead(availabePoints) // mutates available_points
+	body, availablePoints := RandomBody(rand.Intn(MAX_BODY_LENGTH), availablePoints)
 	return Snake{
 		id,
 		name,
@@ -20,6 +21,18 @@ func RandomSnake(name string, available_points map[Point]bool) Snake {
 	}
 }
 
-func randomBody(l int) (error, []Point, Point) {
+func RandomBody(l int, availablePoints []Point) (error, []Point, Point) {
+
+}
+
+func RandomHead(availablePoints []Point) (Point, []Point) {
+	i := rand.Intn(len(availablePoints))
+	chosen := availablePoints[i]
+	availablePoints[i] = availablePoints[len(availablePoints)-1] // replace 'chosen' with the last element
+	availablePoints = availablePoints[:len(availablePoints)-1]   // and delete it
+	return chosen, availablePoints
+}
+
+func RandomBoard(width, height int) Board {
 
 }
